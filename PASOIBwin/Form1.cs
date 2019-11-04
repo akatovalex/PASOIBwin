291738dbd4bdf49981d6b28b9d0a0c4b71fd6f81
akatovalex <akatovaleksander@gmail.com>
2019-11-04 17:18:37 +0300

    github desktop doesn't see changes immediately
    nothing interesting here

--- PASOIBwin/Form1.cs
+++ PASOIBwin/Form1.cs
@@ -16,5 +16,15 @@ namespace PASOIBwin
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
