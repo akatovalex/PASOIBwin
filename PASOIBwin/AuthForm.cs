1626fd20d73f6c550d49482f4aa83791a0e31003
akatovalex <akatovaleksander@gmail.com>
2019-12-27 00:20:49 +0300

    Returned old AuthForm
    A masterpiece of design is back

---
+++ PASOIBwin/AuthForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SecurityAPI;
+
+namespace PASOIBwin
+{
+    public partial class AuthForm : Form
+    {
+        public AuthForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string path = "";
+            //DataBase a = new DataBase(path);
+            labelUsbCheck.Text = "✘";
+            labelUsbCheck.ForeColor = Color.Red;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            labelUsbCheck.Text = "✓";
+            labelUsbCheck.ForeColor = Color.Green;
+        }
+
+
+    }
+}
